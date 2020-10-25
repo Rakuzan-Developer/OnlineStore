90300b1cecc2fd450407ea629719e3753e1466f9
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2020-10-25 13:46:03 +0300

    Add project files.

---
+++ OnlineStore.Core.Common.Contracts/IConfigurationHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Core.Common.Contracts
+{
+    public interface IConfigurationHelper
+    {
+    }
+}
---
+++ OnlineStore.Core/ConfigurationHelper.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Core
+{
+    public class ConfigurationHelper : IConfigurationHelper
+    {
+    }
+}
