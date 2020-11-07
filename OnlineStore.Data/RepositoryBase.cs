577810ac78ef014647603d64246965e1abd08d61
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2020-11-07 18:55:06 +0300

    datav1

---
+++ OnlineStore.Data/RepositoryBase.cs
@@ -0,0 +1,67 @@
+﻿
+using OnlineStore.Data.Contracts;
+using OnlineStore.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OnlineStore.Data
+{
+    public abstract class RepositoryBase<TEntity, TKey> : IRepository<TEntity, TKey>
+        where TEntity : EntityBase<TKey>
+    {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public RepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll(int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable GetAll(int skip, int take, Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  Task<TEntity> GetAsync(TKey Id)
+        {
+            return _dbSet.FindAsync(Id);
+        }
+
+        public Task SaveChangeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
