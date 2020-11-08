79fdc47f6734cdab66dc1955fb554fe9a80452a9
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2020-11-08 19:06:24 +0300

    Business

---
+++ OnlineStore.Business.Contracts/IUserEngine.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Business.Contracts
+{
+    public interface IUserEngine : IBusinessEngine
+    {
+    }
+}
---
+++ OnlineStore.Business/BusinessEngineBase.cs
@@ -0,0 +1,48 @@
+﻿using OnlineStore.Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Business
+{
+    public class BusinessEngineBase
+    {
+        public IConfigurationHelper ConfigurationHelper;
+        private readonly ILogger _logger;
+
+        public BusinessEngineBase()
+        {
+            // TODO: DI Framework eklendikten sonra bu fieldlar initialize edilecek.
+        }
+
+        protected T ExecuteWithExceptionHandledOperation<T>(Func<T> func)
+        {
+            try
+            {
+                var result = func.Invoke();
+
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message, ex);
+                throw;
+            }
+        }
+
+        protected void ExecuteWithExceptionHandledOperation(Action action)
+        {
+            try
+            {
+                action.Invoke();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message, ex);
+                throw;
+            }
+        }
+    }
+}
