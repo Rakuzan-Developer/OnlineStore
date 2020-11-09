b16c19fb902dfaab2524c3d93f8e1698b3423bb8
Rakuzan-Developer <190758+Rakuzan-Developer@users.noreply.github.com>
2020-11-10 01:30:57 +0300

    not okey

---
+++ OnlineStore.Core.Common.Contracts/ApiResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace OnlineStore.Core.Common.Contracts
+{
+    [DataContract]
+    public class ApiResponse<T>
+    {
+        [DataMember]
+        public string Version { get { return "1.0"; } }
+
+        [DataMember]
+        public int StatusCode { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string ErrorMessage { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public T Result { get; set; }
+
+    }
+}
---
+++ OnlineStore.ServiceHost.API/Handlers/ApiResponseHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace OnlineStore.ServiceHost.API.Handlers
+{
+    public class ApiResponseHandler : DelegatingHandler
+    {
+
+        protected override async System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            System.Threading.CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            return BuildApiResponse(request, response);
+        }
+
+        private HttpResponseMessage BuildApiResponse(HttpRequestMessage request, HttpResponseMessage response)
+        {
+            object content = null;
+            string errorMessage = string.Empty;
+        }
+
+        private void ValidateApiResponse(HttpResponseMessage response, ref object content, ref string errorMessage)
+        {
+
+        }
+
+
+    }
+}
